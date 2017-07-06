6771ac7d32c11a442ff34a8f1fa5aaefb7bb5b6e
Wisen Tanasa <wisen@ceilfors.com>
2017-07-06 07:26:00 +0100

    Refactor.

--- test/lambda-spec.js
+++ test/lambda-spec.js
@@ -9,9 +9,9 @@ const {deps, handler} = require('../src/lambda')
 
 describe('lambda', () => {
   it('should return "200" if a user credentials is valid', () => {
-    deps.userService = ({
+    deps.userService = {
       login: () => 'success'
-    })
+    }
     const callback = sinon.spy()
     handler({username: 'any', password: 'any'}, callback)
 
@@ -19,9 +19,9 @@ describe('lambda', () => {
   })
 
   it('should return "404" if a user credentials is invalid', () => {
-    deps.userService = ({
+    deps.userService = {
       login: () => 'invalid'
-    })
+    }
     const callback = sinon.spy()
     handler({username: 'any', password: 'any'}, callback)
 
